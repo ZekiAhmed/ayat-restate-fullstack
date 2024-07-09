c6180532b22a82d393d2312aa1965aaa035d42b0
ZekiAhmed <changezworld3@gmail.com>
2024-07-09 16:27:04 -0700

    created user in auth controller, install prisma, create user model and mongodb url

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,25 @@
-export const register = (req, res) => {
-  // db operations
-  console.log("register endpoint");
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // HASH THE PASSWORD
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  console.log(hashedPassword);
+
+  // CREATE A NEW USER AND SAVE TO DB
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  console.log(newUser);
 };
 
 export const login = (req, res) => {
